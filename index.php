d601d975ec323d2f801fbae4e953ae0a19dd9026
Newstein123 <102919096+Newstein123@users.noreply.github.com>
2024-01-01 19:32:12 +0630

    dir fixed

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader('temp');
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__);
 $twig = new \Twig\Environment($loader,[
     'debug' => true,
     'auto_reload' => true,
 ]);
-
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 $herodata = file_get_contents('temp/data/hero.json');
 // Data to pass to the template
@@ -22,7 +21,7 @@ $data = [
 
 // Render the template
 // $templateFile = 'pages/' . $page . '.html';
-$templateFile = "index.html";
+$templateFile = "public/index.html";
 $template = $twig->load($templateFile);
 echo $template->render($data);
 ?>
